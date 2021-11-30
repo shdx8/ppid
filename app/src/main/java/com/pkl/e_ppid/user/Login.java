e8fef99b27efd2d2f7912f99ceb38a068947eb0b
shdx8 <elyasagus.29@gmail.com>
2021-11-30 22:35:47 +0700

    first commit

---
+++ app/src/main/java/com/pkl/e_ppid/user/Login.java
@@ -0,0 +1,16 @@
+package com.pkl.e_ppid.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pkl.e_ppid.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
