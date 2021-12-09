77185d74be3ee8751c5137c0866c86271832ec21
shdx8 <elyasagus.29@gmail.com>
2021-12-09 10:33:42 +0700

    fitting ui

--- app/src/main/java/com/pkl/e_ppid/MainActivity.java
+++ app/src/main/java/com/pkl/e_ppid/MainActivity.java
@@ -2,6 +2,8 @@ package com.pkl.e_ppid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.parseColor("#03A9F5"));
+        }
     }
 }
